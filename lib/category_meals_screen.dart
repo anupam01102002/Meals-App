8f4c890221c6ce50655053674786292823f8a66a
Anupam Anand <aanand20@iitk.ac.in>
2023-05-19 12:58:19 +0530

    Update

---
+++ lib/category_meals_screen.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import './meal_item.dart';
+import './models/meal.dart';
+import 'dummy_data.dart';
+
+class CategoryMealsScreen extends StatelessWidget {
+  static const routeName = '/category-meals';
+
+  @override
+  Widget build(BuildContext context) {
+    final routeArgs =
+        ModalRoute.of(context).settings.arguments as Map<String, String>;
+    final categoryTitle = routeArgs['title'];
+    final categoryId = routeArgs['id'];
+    final categoryMeals = DUMMY_MEALS.where((Meal) {
+      return Meal.categories.contains(categoryId);
+    }).toList();
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(categoryTitle),
+      ),
+      body: ListView.builder(
+        itemBuilder: (ctx, index) {
+          return MealItem(
+              title: categoryMeals[index].title,
+              imageUrl: categoryMeals[index].imageUrl,
+              affordability: categoryMeals[index].affordability,
+              complexity: categoryMeals[index].complexity,
+              duration: categoryMeals[index].duration);
+        },
+        itemCount: categoryMeals.length,
+      ),
+    );
+  }
+}
